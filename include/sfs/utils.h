725d381e0e9350deda46c26b09f7cb7d4396379d
Jack Barry <jack.barry@live.com>
2023-04-17 22:56:03 +0100

    headers for fs, disk, and sfssh + utils

---
+++ include/sfs/utils.h
@@ -0,0 +1,12 @@
+/* utils.h: Utility Macros */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#define min(a, b)   \
+    (((a) < (b)) ? (a) : (b))
+
+#define max(a, b)   \
+    (((a) > (b)) ? (a) : (b))
+
+#endif
